34580a11654c1ee5c4ac114d650d83ce69ba058e
ashaleyalcantara-cloud <ashaleyalcantara@gmail.com>
2026-09-23 14:40:20 -0300

    feat: adiciona exercício de média

---
+++ Media.py
@@ -0,0 +1,12 @@
+n1 = float(input("Digite a primeira nota"))
+n2 = float(input("Digite a segunda nota"))
+n3 = float(input("Digite a terceira nota"))
+
+media = (n1+n2+n3)/3
+
+if media>=7:
+    print("Aprovado")
+elif 5 <= media <= 6.9:
+    print("Recuperação")
+else:
+    print("Reprovado") 
